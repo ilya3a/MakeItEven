cab29e3d2f2f454f3d756f7e9e083fa2f9306e7e
ronigrr <42034063+ronigrr@users.noreply.github.com>
2019-01-20 20:40:27 +0200

    add stage and arcade btns+ animation the animation is set specificly for thous btns so dont use them on other btn pressd/releas

--- app/src/main/java/com/yoyo/makeiteven/Start_Screen.java
+++ app/src/main/java/com/yoyo/makeiteven/Start_Screen.java
@@ -1,21 +1,28 @@
 package com.yoyo.makeiteven;
 
 import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.transition.ChangeTransform;
+import android.transition.TransitionManager;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class Start_Screen extends Activity {
 
     ImageView game_logo;
     Button stage_mode_btn,arcade_mode_btn;
-
+    ImageButton setting_btn;
+    Boolean isRotated=Boolean.FALSE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,7 @@ public class Start_Screen extends Activity {
         //finding views
         stage_mode_btn =findViewById(R.id.stage_mode_btn);
         arcade_mode_btn=findViewById(R.id.arcade_mode_btn);
+        setting_btn = findViewById(R.id.setting_btn);
         //btn animation
         final Animation btn_press = AnimationUtils.loadAnimation(this,R.anim.btn_pressed);
         final Animation btn_releas= AnimationUtils.loadAnimation(this,R.anim.btn_realeas);
@@ -59,5 +67,38 @@ public class Start_Screen extends Activity {
                 return false;
             }
         });
+        setting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RelativeLayout mCLayout =findViewById(R.id.Main_layout);
+                ChangeTransform changeTransform = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    changeTransform = new ChangeTransform();
+                }
+
+                // Set the duration for transition
+                changeTransform.setDuration(500);
+
+                // Set the transition interpolator
+                changeTransform.setInterpolator(new AccelerateInterpolator());
+
+                // Begin the delayed transition
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    TransitionManager.beginDelayedTransition(mCLayout,changeTransform);
+                }
+
+                // Toggle the button rotation state
+                toggleRotation(setting_btn);
+            }
+        });
+    }
+    protected void toggleRotation(View v){
+        if(isRotated){
+            v.setRotation(0.0f);
+            isRotated = false;
+        }else {
+            v.setRotation(90.0f);
+            isRotated = true;
+        }
     }
 }
