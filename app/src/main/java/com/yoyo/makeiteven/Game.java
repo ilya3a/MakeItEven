9214a53a2941e106d364d145876973273b79505b
ilya aptekar <42389926+ilya3a@users.noreply.github.com>
2019-01-20 13:53:22 +0200

    game algo

---
+++ app/src/main/java/com/yoyo/makeiteven/Game.java
@@ -0,0 +1,210 @@
+package com.yoyo.makeiteven;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.Time;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.SplittableRandom;
+
+public class Game {
+    private int difficulty;
+    private int score;
+    private String hint;
+
+
+    public Game(int difficulty) {
+        this.difficulty = difficulty;
+        this.score = 0;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public int gameGenerator(List<Button> btns) {
+
+        int btn1, btn2, btn3, btn4, sum;
+        Random randNumber = new Random();
+        Time t = new Time(5);
+        t.getTime();
+        int randIdx;
+        hint = new String();
+//        btn1 = randNumber.nextInt(difficulty) + 1;
+//        btn2 = randNumber.nextInt(difficulty) + 1;
+
+//        if (randIdx == 3) {
+//            while (btn1 % btn2 != 0) {
+//                btn2 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = btn1 / btn2;
+//
+//        } else if (randIdx == 2) {
+//            sum = btn1 * btn2;
+//        } else if (randIdx == 1) {
+//            while (btn1 - btn2 < 0) {
+//                btn2 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = btn1 - btn2;
+//        } else {
+//            sum = btn1 + btn2;
+//        }
+//
+//        btn3 = randNumber.nextInt(difficulty) + 1;
+//        randIdx = randNumber.nextInt(4);
+//        if (randIdx == 3) {
+//            while (sum % btn3 != 0) {
+//                btn3 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = sum / btn3;
+//
+//        } else if (randIdx == 2) {
+//            sum = sum * btn3;
+//        } else if (randIdx == 1) {
+//            while (sum - btn3 < 0) {
+//                btn3 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = sum - btn3;
+//        } else {
+//            sum = sum + btn3;
+//        }
+//
+//        btn4 = randNumber.nextInt(difficulty) + 1;
+//        randIdx = randNumber.nextInt(3);
+//        if (randIdx == 3) {
+//            while (sum % btn4 != 0) {
+//                btn4 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = sum / btn4;
+//
+//        } else if (randIdx == 2) {
+//            sum = sum * btn4;
+//        } else if (randIdx == 1) {
+//            while (sum - btn4 < 0) {
+//                btn4 = randNumber.nextInt(difficulty) + 1;
+//            }
+//            sum = sum - btn4;
+//        } else {
+//            sum = sum + btn4;
+//        }
+//
+//
+//
+
+        btn1 = randNumber.nextInt(difficulty) + 1;
+        btn2 = randNumber.nextInt(difficulty) + 1;
+        btn3 = randNumber.nextInt(difficulty) + 1;
+        btn4 = randNumber.nextInt(difficulty) + 1;
+
+        randIdx = randNumber.nextInt(3);
+        if (randIdx == 3) {
+
+            if (btn1 % btn2 == 0) {
+                sum = btn1 / btn2;
+                hint = hint.concat(btn1 + "/" + btn2);
+            } else {
+                sum = btn1 + btn2;
+                hint = hint.concat(btn1 + "+" + btn2);
+            }
+
+
+        } else if (randIdx == 2) {
+            sum = btn1 * btn2;
+            hint = hint.concat(btn1 + "*" + btn2);
+        } else if (randIdx == 1) {
+            if (btn1 - btn2 > 0) {
+                sum = btn1 - btn2;
+                hint = hint.concat(btn1 + "-" + btn2);
+            } else {
+                sum = btn1 + btn2;
+                hint = hint.concat(btn1 + "+" + btn2);
+            }
+        } else {
+            sum = btn1 + btn2;
+            hint = hint.concat(btn1 + "+" + btn2);
+        }
+
+        randIdx = randNumber.nextInt(3);
+
+
+        if (randIdx == 3) {
+            if (sum % btn3 == 0) {
+                sum = sum / btn3;
+                hint = hint.concat("/" + btn3);
+            } else {
+                sum = sum + btn3;
+                hint = hint.concat("+" + btn3);
+            }
+
+
+        } else if (randIdx == 2) {
+            sum = sum * btn3;
+            hint = hint.concat("*" + btn3);
+        } else if (randIdx == 1) {
+            if (sum - btn3 > 0) {
+                sum = sum - btn3;
+                hint = hint.concat("-" + btn3);
+            } else {
+                sum = sum + btn3;
+                hint = hint.concat("+" + btn3);
+            }
+
+        } else {
+            sum = sum + btn3;
+            hint = hint.concat("+" + btn3);
+        }
+        randIdx = randNumber.nextInt(3);
+
+        if (randIdx == 3) {
+            if (sum % btn4 == 0) {
+                sum = sum / btn4;
+                hint = hint.concat("/" + btn4);
+            } else {
+                sum = sum + btn4;
+                hint = hint.concat("+" + btn4);
+            }
+
+
+        } else if (randIdx == 2) {
+            sum = sum * btn4;
+            hint = hint.concat("*" + btn4);
+        } else if (randIdx == 1) {
+            if (sum - btn4 > 0) {
+                sum = sum - btn4;
+                hint = hint.concat("-" + btn4);
+            } else {
+                sum = sum + btn4;
+                hint = hint.concat("+" + btn4);
+            }
+        } else {
+            sum = sum + btn4;
+            hint = hint.concat("+" + btn4);
+        }
+
+        Collections.shuffle(btns);
+        btns.get(0).setText(String.valueOf(btn1));
+        btns.get(1).setText(String.valueOf(btn2));
+        btns.get(2).setText(String.valueOf(btn3));
+        btns.get(3).setText(String.valueOf(btn4));
+
+        if (sum > 100)
+            sum = gameGenerator(btns);
+
+
+        return sum;
+
+
+    }
+}
--- app/src/main/java/com/yoyo/makeiteven/MainActivity.java
+++ app/src/main/java/com/yoyo/makeiteven/MainActivity.java
@@ -2,6 +2,20 @@ package com.yoyo.makeiteven;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView theNumber = findViewById(R.id.the_number);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button start = findViewById(R.id.start_btn);
+
+        final List<Button> btns = new ArrayList<>();
+        btns.add(btn1);
+        btns.add(btn2);
+        btns.add(btn3);
+        btns.add(btn4);
+
+        final Game game = new Game(10);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                theNumber.setText(String.valueOf(game.gameGenerator(btns)+" "+game.getHint()));
+            }
+        });
+
     }
+
 }
+
+
