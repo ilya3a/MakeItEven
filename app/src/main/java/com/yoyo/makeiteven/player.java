5051675e7f93d08d2d19ace098be8b2d07e5469f
ilya aptekar <42389926+ilya3a@users.noreply.github.com>
2019-02-03 23:00:28 +0200

    Merge remote-tracking branch 'remotes/origin/ronald_new_branch'
    # Conflicts:
    #	.idea/caches/gradle_models.ser

--- app/src/main/java/com/yoyo/makeiteven/player.java
+++ app/src/main/java/com/yoyo/makeiteven/player.java
@@ -10,8 +10,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-import static android.content.Context.MODE_PRIVATE;
-
 public class player implements Comparable {
     private String name;
     private Integer score;
