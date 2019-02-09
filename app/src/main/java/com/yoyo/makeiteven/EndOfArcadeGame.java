cc0824664ed99e9700caeefabafd42b691c03036
tamirnahum <tamirnahum@gmail.com>
2019-02-09 21:54:40 +0200

    add "play again button to end of arcade activity"

--- app/src/main/java/com/yoyo/makeiteven/EndOfArcadeGame.java
+++ app/src/main/java/com/yoyo/makeiteven/EndOfArcadeGame.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -23,6 +24,8 @@ public class EndOfArcadeGame extends AppCompatActivity {
 
     EditText nickNameEt;
     TextView finalScoreTv;
+    private int score;
+    private String nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,49 +35,63 @@ public class EndOfArcadeGame extends AppCompatActivity {
         finalScoreTv = findViewById( R.id.final_score_tv );
         nickNameEt = findViewById( R.id.nickname_et );
 
-        final int score = getIntent().getIntExtra( EXTRA_SCORE, 0 );
+        score = getIntent().getIntExtra( EXTRA_SCORE, 0 );
         finalScoreTv.setText( score + "" );
 
-        Button nextBtn = findViewById( R.id.next_btn );
-        nextBtn.setOnClickListener( new View.OnClickListener() {
+
+
+        Button playAgainBtn = findViewById( R.id.play_again_btn );
+        playAgainBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                saveName();
+                GameActivity.startGameActivity( EndOfArcadeGame.this, ArcadeGame.TYPE );
+                finish();
+            }
+        } );
+
+
+        ImageButton scoreBoardBtn = findViewById( R.id.scoreBoard_ib );
+        scoreBoardBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveName();
                 Intent intent = new Intent( EndOfArcadeGame.this, ScoreBoardActivity.class );
-                final String nickname = nickNameEt.getText().toString();
-
-                ScoreBoard scoreBoard = new ScoreBoard( nickname, score );
-
-                // get instance of shared prefs with relevant key
-                SharedPreferences sharedPref = getSharedPreferences( DataStore.PREFS, MODE_PRIVATE );
-                String scoreBoardJsonList = sharedPref.getString( DataStore.SHARED_KEY_SCORE_BOARD, "[]" );
-
-                Gson gson = new Gson();
-                Type type = new TypeToken<ArrayList<ScoreBoard>>() {
-                }.getType();
-
-                // convert the json String to Java ArrayList object
-                ArrayList<ScoreBoard> scoreBoards = gson.fromJson( scoreBoardJsonList, type );
-
-                // manipulate the java arrayList
-                scoreBoards.add( scoreBoard );
-
-                Collections.sort( scoreBoards, scoreBoard );
-
-                // convert the array list back to json string
-                String jsonUpdatedScoreBoard = gson.toJson( scoreBoards );
-
-                SharedPreferences.Editor editor = sharedPref.edit();
-
-                // save the json string back to the shared prefs
-                editor.putString( DataStore.SHARED_KEY_SCORE_BOARD, jsonUpdatedScoreBoard );
-                editor.apply();
-
                 startActivity( intent );
                 finish();
             }
         } );
 
 
+    }
+
+    private void saveName() {
+        nickname = nickNameEt.getText().toString();
+        ScoreBoard scoreBoard = new ScoreBoard( nickname, score );
+        // get instance of shared prefs with relevant key
+        SharedPreferences sharedPref = getSharedPreferences( DataStore.PREFS, MODE_PRIVATE );
+        String scoreBoardJsonList = sharedPref.getString( DataStore.SHARED_KEY_SCORE_BOARD, "[]" );
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<ScoreBoard>>() {
+        }.getType();
+
+        // convert the json String to Java ArrayList object
+        ArrayList<ScoreBoard> scoreBoards = gson.fromJson( scoreBoardJsonList, type );
+
+        // manipulate the java arrayList
+        scoreBoards.add( scoreBoard );
+
+        Collections.sort( scoreBoards, scoreBoard );
+
+        // convert the array list back to json string
+        String jsonUpdatedScoreBoard = gson.toJson( scoreBoards );
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        // save the json string back to the shared prefs
+        editor.putString( DataStore.SHARED_KEY_SCORE_BOARD, jsonUpdatedScoreBoard );
+        editor.apply();
     }
 }
 
